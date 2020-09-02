49ae0dbe31c1264182dc71bdbdbd45c8ea8de02e
Dylan Enloe <enloed@gmail.com>
2020-09-01 18:45:38 -0700

    Add Profile Block support to EEPROM Manager

--- RFUSB_to_DB15/EepromManager.cpp
+++ RFUSB_to_DB15/EepromManager.cpp
@@ -2,6 +2,7 @@
 // Created by Kitsune on 8/26/2020.
 //
 #include <EEPROM.h>
+#include <SPI.h>
 
 #include "device_descriptor.h"
 #include "EepromManager.h"
@@ -17,6 +18,7 @@ EepromManager::EepromManager() {
   if(EEPROM.read(3) != EEPROM_VERISON) {
     Initialize();
   }
+  //Initialize();
 }
 
 /**
@@ -24,7 +26,7 @@ EepromManager::EepromManager() {
  */
 void EepromManager::Initialize() {
   // Populate initial profiles
-  GenerateInitialProfiles();
+  InitializeProfileBlocks();
 
   //Create the header last only after everything completes
   EEPROM.update(0, EEPROM_BYTE_0);
@@ -38,7 +40,7 @@ void EepromManager::Initialize() {
  * @return The profile number
  */
 uint8_t EepromManager::LoadCurrentProfile() {
-  return EEPROM.read(CURRENT_PROFILE_ADDR);
+  return EEPROM.read(GetProfileBlockStartAddr() + CURRENT_PROFILE_ADDR);
 }
 
 /**
@@ -46,7 +48,7 @@ uint8_t EepromManager::LoadCurrentProfile() {
  * @param num The Current Profile Number
  */
 void EepromManager::SaveCurrentProfile(uint8_t num) {
-  EEPROM.update(CURRENT_PROFILE_ADDR, num);
+  EEPROM.update(GetProfileBlockStartAddr() + CURRENT_PROFILE_ADDR, num);
 }
 
 /**
@@ -55,7 +57,7 @@ void EepromManager::SaveCurrentProfile(uint8_t num) {
  * @param profile The profile object to load the profile into
  */
 void EepromManager::LoadProfile(uint8_t num, Profile &profile) {
-  int start_addr = PROFILE_START_ADDR + (num * PROFILE_SIZE);
+  int start_addr = GetProfileStartAddr() + (num * PROFILE_SIZE);
   if (num >= MAX_PROFILES) return;
 
   for(uint8_t i;i < NUM_BUTTON_BINDINGS; i++) {
@@ -64,12 +66,58 @@ void EepromManager::LoadProfile(uint8_t num, Profile &profile) {
 }
 
 void EepromManager::SaveProfile(uint8_t num, Profile &profile) {
-  int start_addr = PROFILE_START_ADDR + (num * PROFILE_SIZE);
+  int start_addr = GetProfileStartAddr() + (num * PROFILE_SIZE);
   if (num >= MAX_PROFILES) return;
 
   for(uint8_t i;i < NUM_BUTTON_BINDINGS; i++) {
     EEPROM.update(start_addr + i, profile.bindings[i]);
   }
+}
+
+void EepromManager::SetCurrentController(uint16_t vid, uint16_t pid) {
+  uint8_t found = 0;
+  uint16_t addr = PROFILE_START_ADDR;
+
+  for(uint8_t i = 0; i < MAX_PROFILE_BLOCKS; i++) {
+    if(vid == Read16Bit(addr) && pid == Read16Bit(addr + 2)) {
+      Serial.print("Found Controller.  Using Profile Block: ");
+      Serial.println(i);
+      found = 1;
+      cur_profile_block = i;
+      break;
+    }
+    addr += PROFILE_SIZE;
+  }
+  if(!found) {
+    cur_profile_block = EEPROM.read(NEXT_PROFILE_BLOCK_ADDR);
+    Serial.print("Could Not find Controller. Generating Profile Block:");
+    Serial.println(cur_profile_block);
+    // Write the vid and pid to EEPROM
+    Write16Bit(GetProfileBlockStartAddr(), vid);
+    Write16Bit(GetProfileBlockStartAddr() + 2, pid);
+    // Set the current profile to 0
+    EEPROM.update(GetProfileBlockStartAddr() + 4, 0);
+    // Generate Default profiles
+    GenerateInitialProfiles();
+    // Update Next Profile Block.  Mod block by 10 to loop around
+    EEPROM.update(NEXT_PROFILE_BLOCK_ADDR, (cur_profile_block + 1) % 10);
+  }
+}
+
+/**
+ * Calculates the start address for profiles in the current profile block
+ * @return The Address of byte 0 of profile 0
+ */
+inline uint16_t EepromManager::GetProfileStartAddr() {
+  return GetProfileBlockStartAddr() + PROFILE_BLOCK_HEADER_SIZE;
+}
+
+/**
+ * Calculates the Address of byte 0 of the current profile block
+ * @return The address of byte 0 of the current profile block
+ */
+inline uint16_t EepromManager::GetProfileBlockStartAddr() {
+  return PROFILE_START_ADDR + (cur_profile_block * PROFILE_SIZE);
 }
 
 void EepromManager::GenerateInitialProfiles() {
@@ -91,12 +139,31 @@ void EepromManager::GenerateInitialProfiles() {
   // Profile 3 is Profile 2 with the rows swapped
   SwapProfileRows(profile);
   SaveProfile(3, profile);
+}
 
-  // Profile 4 - n haven't been decided yet and are default
-  GenerateDefaultProfile(profile);
-  for(uint8_t i = 4; i < MAX_PROFILES; i++) {
-    SaveProfile(i, profile);
+void EepromManager::InitializeProfileBlocks() {
+  uint16_t addr = PROFILE_START_ADDR;
+  for(int i = 0; i < MAX_PROFILE_BLOCKS; i++) {
+    // Set the VID, PID and Current profile of the Profile Block to Zero
+    EEPROM.update(addr, 0);
+    EEPROM.update(addr + 1, 0);
+    EEPROM.update(addr + 2, 0);
+    EEPROM.update(addr + 3, 0);
+    EEPROM.update(addr + 4, 0);
+    addr += PROFILE_SIZE;
   }
+}
+
+uint16_t EepromManager::Read16Bit(uint16_t addr) {
+  uint16_t value;
+  value = (EEPROM.read(addr + 1) << 8);
+  value |= EEPROM.read(addr);
+  return value;
+}
+
+void EepromManager::Write16Bit(uint16_t addr, uint16_t value) {
+  EEPROM.update(addr, value);
+  EEPROM.update(addr + 1, value >> 8);
 }
 
 /**
